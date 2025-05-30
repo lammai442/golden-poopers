3ff8e67488fe74c807ac8c1402b0b2ba7ad67d6b
Tivva34 <gemtime1991@gmail.com>
2025-05-30 11:37:33 +0200

    Skapade authorizeKey och authorizeUser

--- server/middlewares/authorize.js
+++ server/middlewares/authorize.js
@@ -1,3 +1,35 @@
-// function authorizeKey?
+import Key from '../models/key.js';
 
-// function authorizeUser?
+
+/* Checka om en giltig api nyckel finns */
+export const authorizeKey = async (req, res, next) => {
+    try {
+        const apiKey = req.query.key || req.headers['api-key'];
+        
+        if (!apiKey) {
+            return res.status(401).json({ success: false, message: 'API key is required' });
+        }
+
+        const validKey = await Key.findOne({ key: apiKey });
+        if (!validKey) {
+            return res.status(403).json({ success: false, message: 'API key Invalid' });
+        }
+
+        console.log(`API key: ${apiKey} is valid`);
+        
+        next();
+    } catch (err) {
+        console.error('Error in middleware-authorizeKey', err);
+        next(err);
+    }
+};       
+
+
+/* kolla om användaren är inloggad glöm ej global.user måste sättas */
+export const authorizeUser = (req, res, next) => {
+    if (!global.user) {
+        return res.status(401).json({ success: false, message: 'Unauthorized user' });
+    }
+    
+    next();
+};
