f85ed4d7a054b2637e49e0e2bbd52eb2a4ab6a0d
Tivva34 <gemtime1991@gmail.com>
2025-05-30 11:55:33 +0200

    fixade ett fel

--- server/middlewares/authorize.js
+++ server/middlewares/authorize.js
@@ -4,7 +4,7 @@ import Key from '../models/key.js';
 /* Checka om en giltig api nyckel finns */
 export const authorizeKey = async (req, res, next) => {
     try {
-        const apiKey = req.query.key || req.headers['api-key'];
+        const apiKey = req.query.key;
         
         if (!apiKey) {
             return res.status(401).json({ success: false, message: 'API key is required' });
@@ -16,7 +16,7 @@ export const authorizeKey = async (req, res, next) => {
         }
 
         console.log(`API key: ${apiKey} is valid`);
-        
+
         next();
     } catch (err) {
         console.error('Error in middleware-authorizeKey', err);
@@ -30,6 +30,5 @@ export const authorizeUser = (req, res, next) => {
     if (!global.user) {
         return res.status(401).json({ success: false, message: 'Unauthorized user' });
     }
-    
     next();
 };
